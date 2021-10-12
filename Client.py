1e3db9caaf54e2bdbdd6b1adfd27b014f9d81ed3
Mariana Rodrigues <54726918+marirodriguesxx@users.noreply.github.com>
2021-10-12 06:10:18 -0300

    comentários + falta fazer o /get

--- Client.py
+++ Client.py
@@ -1,18 +1,51 @@
+##Trabalho 2 - INF 452
+# Autores: Mariana Rodrigues de Sant'Ana(Es98875) e Rafael Rocha(Es90668) 
+
 import socket
 import threading
 import time
-import sys
 
 HOST = '127.0.0.1'
 PORT = 20000
 
+def send_File(fileName):    
+    # creating a socket implementing TCP for sending files
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    tcp.connect((socket.gethostname(), PORT))
+    f = open(fileName, 'rb')
+    read_file = f.read(1024)
+    while (read_file):
+        tcp.send(read_file)
+        read_file = f.read(1024)
+    f.close()
+    tcp.shutdown(socket.SHUT_WR)
+    tcp.close()
+
+def receive_File(fileName):
+        try:
+            # creating a socket implementing TCP for sending and receiving files
+            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            tcp.bind((HOST, PORT))  
+            f = open("[copy]" + fileName, 'wb')
+            tcp.listen(5)  # Now wait for client connection.
+            while True:
+                #accepting client connection request
+                c, address = tcp.accept()  
+                cliente_file = c.recv(1024)
+                while (cliente_file):
+                    f.write(cliente_file)
+                    cliente_file = c.recv(1024)
+                f.close()
+                print("Done Receiving")
+                c.close()
+        except:
+            tcp.close()
 
 #will run as long as the client maintains its connection.
 def client_listen():
     while True:
         msgs, serv = c.recvfrom(1024)
         print(msgs)
-
 
 #will be executed as long as the client does not close the connection with the command "/bye"
 def client_send():
@@ -26,35 +59,21 @@ def client_send():
         if msg == '/bye':
             break
         if '/file' in msg:
-            threading.Thread(
-                target=sendFileTCP,
-                args=(msg.replace('/file ', ''), ),
-            ).start()
+            print('Enviando arquivo')
+            send_file = threading.Thread(target=send_File, args=(msg.replace('/file ', ''), ),)
+            send_file.start()
+        if '/get' in msg:
+            print('Pegar arquivo')
+            # receive_File = threading.Thread(target=send_File, args=(msg.replace('/file ', ''), ),)
+            # receive_File.start()
     time.sleep(5)
     c.close()
     return
 
 
-def sendFileTCP(fileName):
-    s = socket.socket(
-        socket.AF_INET,
-        socket.SOCK_STREAM,
-    )  # Create a socket object
-
-    s.connect((socket.gethostname(), PORT))
-    # s.send(("Enviando arquivo " + fileName + "...").encode())
-    f = open(fileName, 'rb')
-    l = f.read(1024)
-    while (l):
-        s.send(l)
-        l = f.read(1024)
-    f.close()
-    s.shutdown(socket.SHUT_WR)
-    s.close()
-
-
+# creating a socket implementing UDP for sending and receiving normal messages
 c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-to = (HOST, PORT)
+to = (HOST, PORT) 
 
 #the client will listen to the server in parallel sending messages, to receive messages from other connected clients
 listen = threading.Thread(target=client_listen)
--- Server.py
+++ Server.py
@@ -1,5 +1,5 @@
-#Trabalho 2 - INF 452
-## Autores: Mariana Rodrigues de Sant'Ana(Es98875) e Rafael Rocha(Es90668)                                                                                       ##
+##Trabalho 2 - INF 452
+# Autores: Mariana Rodrigues de Sant'Ana(Es98875) e Rafael Rocha(Es90668)                                                                                       ##
 
 import socket
 import threading
@@ -11,39 +11,47 @@ usernames = []
 users_info = {}
 
 
+def list_conected_clients(msg_list, request):
+    s.sendto(msg_list, request)
+
+def send_File(fileName):    
+    # creating a socket implementing TCP for sending files
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    tcp.connect(('', PORT))
+    f = open(fileName, 'rb')
+    read_file = f.read(1024)
+    while (read_file):
+        tcp.send(read_file)
+        read_file = f.read(1024)
+    f.close()
+    tcp.shutdown(socket.SHUT_WR)
+    tcp.close()
+
+def receive_File(fileName):
+        try:
+            # creating a socket implementing TCP for sending and receiving files
+            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            tcp.bind((HOST, PORT))  
+            f = open("[copy]" + fileName, 'wb')
+            tcp.listen(5)  # Now wait for client connection.
+            while True:
+                #accepting client connection request
+                c, address = tcp.accept()  
+                cliente_file = c.recv(1024)
+                while (cliente_file):
+                    f.write(cliente_file)
+                    cliente_file = c.recv(1024)
+                f.close()
+                print("Done Receiving File")
+                c.close()
+        except:
+            tcp.close()
+        
+
 def server_send(msg, sender):
     for info, user in list(users_info.items()):
         if info != sender:  #broadcast: send to everyone except the sender
             s.sendto(msg, info)
-
-
-def list_conected_clients(msg_list, request):
-    s.sendto(msg_list, request)
-
-
-def receiveFileTCP(fileName):
-    try:
-        tcp = socket.socket(socket.AF_INET,
-                            socket.SOCK_STREAM)  # Create a socket object
-        tcp.bind((HOST, PORT))  # Bind to the port
-        f = open("[copy]" + fileName, 'wb')
-        tcp.listen(5)  # Now wait for client connection.
-        while True:
-            c, addr = tcp.accept()  # Establish connection with client.
-            print('Got connection from', addr)
-            print("Receiving...")
-            l = c.recv(1024)
-            while (l):
-                print("Receiving...")
-                f.write(l)
-                l = c.recv(1024)
-            f.close()
-            print("Done Receiving")
-            c.send(('Thank you for connecting').encode())
-            c.close()
-    except:
-        tcp.close()
-
 
 def server_listen():
     while True:
@@ -52,8 +60,7 @@ def server_listen():
         #If it's a new client connecting to the server, we add him and his information to the list and send the login notification to the others.
         if client not in clients:
             user = msg.decode() + ' entrou'
-            print(user)
-            server_send(user.encode(), client)
+            server_send(user.encode(),client)
             usernames.append(msg)
             clients.append(client)
             users_info[client] = msg
@@ -62,39 +69,41 @@ def server_listen():
             #While the server listens to the client, it sends the broadcast in parallel
             if msg.decode() == '/bye':
                 msg_bye = users_info[client].decode() + ' saiu'
-                print(msg_bye)
-                server_send(msg_bye.encode(), client)
+                server_send(msg_bye.encode(),client)
                 usernames.remove(users_info[client])
                 clients.remove(client)
-                if not clients:
+                if not clients: #if there are no more "online" clients, the server is closed
                     break
             #If the list of logged in users is requested, we transform the list of users so far and send it in string format only to the one who requested the list
             elif msg.decode() == '/list':
                 msg_list = ','.join(str(x) for x in usernames)
                 s.sendto(('Clientes conectados: ' + msg_list).encode(), client)
             elif '/file' in msg.decode():
-                print(msg.decode())
-                threading.Thread(
-                    target=receiveFileTCP,
-                    args=(msg.decode().replace('/file ', ''), ),
-                ).start()
-
-                msg_chat = users_info[client].decode(
-                ) + ' enviou um arquivo: ' + msg.decode().replace(
-                    '/file ', '')  #variable to identify sender
-                server_send(msg_chat.encode(), client)
+                #notifying other customers a notice that a file has been sent
+                msg_file = users_info[client].decode() + ' enviou um arquivo: ' + msg.decode().replace('/file ', '')  #variable to identify who sent the file
+                send_file_notification = threading.Thread(target=server_send, args= (msg_file.encode(),client))
+                send_file_notification.start()
+                #starting to receive files
+                file = threading.Thread(target=receive_File, args=(msg.decode().replace('/file ', ''), ),)
+                file.start()
+            elif '/get' in msg.decode():
+                print('Pegando arquivo')
+                #starting to send the files
+                # file = threading.Thread(target=send_File, args=(msg.decode().replace('/file ', ''), ),)
+                # file.start()
             else:
-                msg_chat = users_info[client].decode(
-                ) + ' disse: ' + msg.decode()  #variable to identify sender
-                server_send(msg_chat.encode(), client)
+                msg_chat = users_info[client].decode() + ' disse: ' + msg.decode()  #variable to identify who sent the message
+                server_send(msg_chat.encode(),client)
     s.close()
     return
 
 
 print('Aguardando conexão')
+# creating a socket implementing UDP for receiving normal messages and broadcast
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 orig = (HOST, PORT)
 s.bind(orig)
+ 
 
 listen = threading.Thread(target=server_listen)
 listen.start()
